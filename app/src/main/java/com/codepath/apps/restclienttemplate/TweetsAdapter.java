a77cb10aa0b2fb7dcb42390454a62268856c6c55
mmmz21 <emmawilcox121@gmail.com>
2020-02-17 16:58:20 -0800

    User can refresh tweets timileine by pulling down to refresh; required stories completed

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -48,6 +48,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
+    // Clean all elements of the recycler
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> list) {
+        tweets.addAll(list);
+        notifyDataSetChanged();
+    }
+
     // pass in the context and list of tweets
     // for each row, inflate the layout
     // bind values based on the position of the element
